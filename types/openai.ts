e378cbc39fa558d0e38746caaa699f59790c724e
Martin Bowling <martinbowling@gmail.com>
2023-06-14 12:00:00 -0400

    Update openai.ts
    update model for token length

--- types/openai.ts
+++ types/openai.ts
@@ -1,12 +1,22 @@
+import { OPENAI_API_TYPE } from '../utils/app/const';
+
 export interface OpenAIModel {
   id: string;
   name: string;
+  maxLength: number; // maximum length of a message
+  tokenLimit: number;
 }
 
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
+  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
+  GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
+  GPT_4_32K = 'gpt-4-32k',
 }
+
+// in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
+export const fallbackModelID = OpenAIModelID.GPT_3_5;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
